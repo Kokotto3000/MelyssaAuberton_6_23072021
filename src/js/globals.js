5b109c69c11e42df1fa628155fac26c99591c767
Mélyssa Auberton <melyssa.auberton@gmail.com>
2021-08-04 00:40:58 +0200

    lightbox moche mais fonctionnelle :)))

--- src/js/globals.js
+++ src/js/globals.js
@@ -4,3 +4,4 @@ export const PHOTOGRAPHER_MEDIAS= document.getElementById('photographer-medias')
 export const PHOTOGRAPHER_LIKES= document.getElementById('likes');
 export const PHOTOGRAPHER_PRICE= document.getElementById('price');
 export const FORM= document.querySelector("form");
+export const LIGHTBOX= document.getElementById('lightbox');
