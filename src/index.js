af5b15d391aee724249a8f51a2b2f6740877455a
Mélyssa Auberton <melyssa.auberton@gmail.com>
2021-08-17 16:00:54 +0200

    résolution de problèmes liés à la navigation au clavier mais reste bug au lecteur d'écran...

--- src/index.js
+++ src/index.js
@@ -38,6 +38,12 @@ let sliderArray= [];
 
 let likes= 0;
 
+// focus piégé dans la modal
+let tabbableElements = 'a[href], area[href], input:not([disabled]),' +
+	'select:not([disabled]), textarea:not([disabled]),' +
+	'button:not([disabled]), iframe, object, embed, *[tabindex],' +
+	'*[contenteditable]';
+
 if(NAV){
     NAV.appendChild(navList);
     tags.forEach(tag=> {
@@ -71,13 +77,6 @@ function init(){
             const photographerCard = new Photographer(photographer);
             photographerCard.updatePhotographerCards();
         });
-        
-
-
-
-
-
-        
     } 
     /*// création de l'évènement sur les boutons tag
     const TAG_FILTERS= document.querySelectorAll('.tag');
@@ -128,20 +127,36 @@ function displayPhotographerPresentation(){
                     // modal events
                     modalBtn.addEventListener("click", ()=> {
                         modalbg.style.display = "block";
-                        MAIN_WRAPPER.setAttribute("aria-hidden", "true");
-                        MAIN_WRAPPER.style.overflow= "hidden";
-                        modalbg.setAttribute("aria-hidden", "false");
+                        modalbg.setAttribute("aria-modal", "true");
+                        keepFocus(modalbg);
+                        // MAIN_WRAPPER.setAttribute("aria-hidden", "true");
+                        // MAIN_WRAPPER.style.overflow= "hidden";
+                        // modalbg.setAttribute("aria-hidden", "false");
                         modalCloseBtn.focus();
                     });
                     modalCloseBtn.addEventListener("click", ()=> {
                         modalbg.style.display = "none";
-                        MAIN_WRAPPER.setAttribute("aria-hidden", "false");
-                        MAIN_WRAPPER.style.overflow= "scroll";
-                        modalbg.setAttribute("aria-hidden", "true");
+                        modalbg.setAttribute("aria-modal", "false");
+                        // MAIN_WRAPPER.setAttribute("aria-hidden", "false");
+                        // MAIN_WRAPPER.style.overflow= "scroll";
+                        // modalbg.setAttribute("aria-hidden", "true");
                         contact.resetForm();
                         isValidFirst= isValidLast= isValidMail= false;
                         modalBtn.focus();
                     });
+
+                    contact.inputs.first.addEventListener("change", ()=>{
+                        contact.stringValidation("first");
+                    });
+
+                    contact.inputs.last.addEventListener("change", ()=>{
+                        contact.stringValidation("last");
+                    });
+
+                    contact.inputs.email.addEventListener("change", ()=>{
+                        contact.emailValidation();
+                    });
+
                     FORM.addEventListener("submit", e => {
                         e.preventDefault();
                         contact.stringValidation("first") ? isValidFirst= true : false;
@@ -218,7 +233,7 @@ function updatePhotographerMedias(id, filter){
 
     //LIGHTBOX
     
-    const MEDIAS= document.querySelectorAll('.photographer-media');
+    const MEDIAS= document.querySelectorAll('.photographer-media__link');
     // const LIGHTBOX= document.getElementById('lightbox');
     // console.log(sliderArray);
     MEDIAS.forEach(media => media.addEventListener('click', ()=> {
@@ -226,9 +241,18 @@ function updatePhotographerMedias(id, filter){
         const sliderIndex= sliderArray.map(sliderId => sliderId.id).indexOf(Number(media.id));
         // console.log(sliderIndex);
         LIGHTBOX.style.display= 'block';
+        LIGHTBOX.setAttribute("aria-modal", "true");
+        LIGHTBOX.focus();
+        // Call the function when the part of the page gets focus
+        // var modal = document.querySelector('.modal');
+        
+        
         // console.log(sliderIds.indexOf(Number(media.id)));
         const lightbox= new Lightbox(sliderArray);
         lightbox.displayLightbox(sliderIndex);
+        console.log(lightbox.lightbox.children[0].firstChild);
+        keepFocus(lightbox.lightbox);
+        lightbox.lightbox.children[0].firstChild.focus();
     }));
 }
 
@@ -238,7 +262,7 @@ function updatePhotographerMedias(id, filter){
 if(BUTTON) BUTTON.addEventListener('click', openDropdown);
 
 function openDropdown(e){
-    DROPDOWN.innerHTML= `<ul id="exp_elem_list" arialabelledby="exp_elem" aria-activedescendant= "exp_elem_${e.target.textContent}" role="listbox" aria-expended="true">` + dropdownElements.map(element => `<li role="option" id="exp_elem_${element}" tabindex= "0" class="button photographer-medias__filter-dropdown-button">${element}</li>`).join('') + `</ul>`;
+    DROPDOWN.innerHTML = `<ul id="exp_elem_list" arialabelledby="exp_elem" aria-activedescendant= "exp_elem_${e.target.textContent}" role="listbox" aria-expended="true">` + dropdownElements.map(element => `<li role="option" id="exp_elem_${element}" tabindex= "0" class="button photographer-medias__filter-dropdown-button">${element}</li>`).join('') + `</ul>`;
     const DROPDOWN_BUTTONS= document.querySelectorAll('.photographer-medias__filter-dropdown-button');
     DROPDOWN_BUTTONS.forEach(button=> {
         button.addEventListener('click', filterDropdown);
@@ -304,6 +328,52 @@ if(PHOTOGRAPHERS_SECTION){
             scrollButton.innerText= "Passer au contenu";
             PHOTOGRAPHERS_SECTION.appendChild(scrollButton);
             isScrolling= true;
+            scrollButton.focus();
         }        
     });
 }
+
+
+// // focus piégé dans la modal
+// const tabbableElements = 'a[href], area[href], input:not([disabled]),' +
+// 	'select:not([disabled]), textarea:not([disabled]),' +
+// 	'button:not([disabled]), iframe, object, embed, *[tabindex],' +
+// 	'*[contenteditable]';
+
+function keepFocus(context) {
+	const allTabbableElements = context.querySelectorAll(tabbableElements);
+	const firstTabbableElement = allTabbableElements[0];
+	const lastTabbableElement = allTabbableElements[allTabbableElements.length - 1];
+
+	function keyListener(event) {
+		let keyCode = event.which || event.keyCode; // Get the current keycode
+
+		// Polyfill to prevent the default behavior of events
+		event.preventDefault = event.preventDefault || function () {
+			event.returnValue = false;
+		};
+
+		// If it is TAB
+		if (keyCode === 9) {
+
+			// Move focus to first element that can be tabbed if Shift isn't used
+			if (event.target === lastTabbableElement && !event.shiftKey) {
+				event.preventDefault();
+				firstTabbableElement.focus();
+
+			// Move focus to last element that can be tabbed if Shift is used
+			} else if (event.target === firstTabbableElement && event.shiftKey) {
+				event.preventDefault();
+				lastTabbableElement.focus();
+			}
+		}
+	};
+
+	context.addEventListener('keydown', keyListener, false);
+};
+
+// Call the function when the part of the page gets focus
+// var modal = document.querySelector('.modal');
+// keepFocus(modal);
+
+// modal.focus();
--- src/js/Lightbox.js
+++ src/js/Lightbox.js
@@ -9,13 +9,14 @@ export class Lightbox{
     displayLightbox(index){
         this.index= index;
         if(this.slider[this.index].image){
-            this.lightbox.innerHTML= `<div class='lightbox-content'><button class="lightbox-content__close-button" aria-label="Close dialog"></button><figure><img src="../assets/images/${this.slider[this.index].photographerId}/${this.slider[this.index].image}" alt="${this.slider[this.index].alt}" /><figcaption>${this.slider[this.index].title}</figcaption></figure><div role="link" class="lightbox-content__previous-button" aria-label="Previous image"></div><div role="link" class="lightbox-content__next-button" aria-label="Next image"></div></div>`;
+            this.lightbox.innerHTML= `<div class='lightbox-content'><button role="button" class="lightbox-content__close-button" aria-label="Close dialog"></button><figure><img src="../assets/images/${this.slider[this.index].photographerId}/${this.slider[this.index].image}" alt="${this.slider[this.index].alt}" /><figcaption>${this.slider[this.index].title}</figcaption></figure><button role="button" class="lightbox-content__previous-button" aria-label="Previous image"></button><button role="button" class="lightbox-content__next-button" aria-label="Next image"></button></div>`;
         }else if(this.slider[this.index].video){
-            this.lightbox.innerHTML= `<div class='lightbox-content'><button class="lightbox-content__close-button" aria-label="Close dialog"></button><video controls><source src="../assets/images/${this.slider[this.index].photographerId}/${this.slider[this.index].video}" alt="${this.slider[this.index].alt}" /><figcaption>${this.slider[this.index].title}</figcaption></video><div role="link" class="lightbox-content__previous-button" aria-label="Previous image"></div><div role="link" class="lightbox-content__next-button" aria-label="Next image"></div></div>`;
+            this.lightbox.innerHTML= `<div class='lightbox-content'><button role="button" class="lightbox-content__close-button" aria-label="Close dialog"></button><video controls><source src="../assets/images/${this.slider[this.index].photographerId}/${this.slider[this.index].video}" alt="${this.slider[this.index].alt}" /><figcaption>${this.slider[this.index].title}</figcaption></video><button role="button" class="lightbox-content__previous-button" aria-label="Previous image"></button><button role="button" class="lightbox-content__next-button" aria-label="Next image"></button></div>`;
         }
         
         const LIGHTBOX_CLOSE= document.querySelector('.lightbox-content__close-button').addEventListener('click', ()=> {
-            LIGHTBOX.style.display= "none";
+            this.lightbox.style.display= "none";
+            this.lightbox.setAttribute("aria-modal", "false");
         });
         const LIGHTBOX_PREVIOUS= document.querySelector('.lightbox-content__previous-button').addEventListener('click', ()=> {
             // console.log('previous');
@@ -37,7 +38,8 @@ export class Lightbox{
                 this.index <= 0 ? this.index= this.slider.length - 1 : this.index--;
                 this.displayLightbox(this.index);
             }else if(e.code === "Escape"){
-                LIGHTBOX.style.display= "none";
+                this.lightbox.style.display= "none";
+                this.lightbox.setAttribute("aria-modal", "false");
             }
         });
 
